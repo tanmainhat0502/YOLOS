42881f8e85dda239ab8671669501f098443d7638
Nhật Tân <khanhu161217@gmail.com>
2025-07-16 04:40:26 +0000

    hehe

--- train_dis.sh
+++ train_dis.sh
@@ -1,14 +1,14 @@
 !python -m torch.distributed.launch --nproc_per_node=2 --master_port=12345 \
     --use_env main.py \
     --coco_path /kaggle/working/data4training \
-    --batch_size 1 \
+    --batch_size 2 \
     --lr 2.5e-5 \
-    --epochs 4 \
+    --epochs 100 \
     --backbone_name small_dWr \
     --pre_trained /kaggle/working/YOLOS/deit_s_dWr_300.pth \
     --eval_size 600 \
     --init_pe_size 512 864 \
     --mid_pe_size 512 864 \
     --output_dir run \
-    --wandb_project EmoticGender-Detection \
+    --wandb_project YOlos-Detection \
     --wandb_name yolos_ver2
